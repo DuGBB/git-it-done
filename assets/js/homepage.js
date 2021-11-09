31fecd6af9015446b417268fb654140c3bab0e87
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-08 21:46:40 -0600

    add error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,10 +5,19 @@ var getUserRepos = function(user) {
 
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        displayRepos(data, user);
-      });
+    fetch(apiUrl)
+      .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+              displayRepos(data, user);
+              });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+
+    .catch(function(errror) {
+        alert("Unable to connet to GitHub");
     });
   };
 
@@ -29,6 +38,11 @@ var getUserRepos = function(user) {
 }
 
 var displayRepos = function(repos, searchTerm) {
+
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;

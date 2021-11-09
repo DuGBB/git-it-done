666d5306b988a0347060ba5d524c1550ba0e7798
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-08 18:16:33 -0600

    update request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,17 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        response.json().then(function(data) {
-          console.log(data);
-        })
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
     });
+  };
+    //console.log("outside");
+    //var response = fetch("https://api.github.com/users/octocat/repos");
+    //console.log(response);
 
-    console.log("outside");
-    var response = fetch("https://api.github.com/users/octocat/repos");
-    console.log(response);
-};
 
-getUserRepos();
+getUserRepos("dugbb");

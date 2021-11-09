cab73f66cf4e0460037e22fca3cf100e6294d922
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-08 19:27:03 -0600

    handle form submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,28 @@
 var getUserRepos = function(user) {
-    // format the github api url
+
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-    // make a request to the url
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
         console.log(data);
       });
     });
   };
-    //console.log("outside");
-    //var response = fetch("https://api.github.com/users/octocat/repos");
-    //console.log(response);
 
+  var userFormEl = document.querySelector("#user-form");
+  var nameInputEl = document.querySelector("#username");
 
-getUserRepos();
+  var formSubmitHandler = function(event) {
+      event.preventDefault();
+      
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+      getUserRepos(username);
+      nameInputEl.value = "";
+  } else {
+      alert("Please enter a GitHub username");
+  }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
